a3a3ab7078fd2a26795567abef63cdcca24105e8
mark <mah6@williams.edu>
2018-07-29 00:17:51 +0900

    documentation

--- System/Random/Reservoir.hs
+++ System/Random/Reservoir.hs
@@ -1,48 +1,71 @@
 {-# LANGUAGE BangPatterns #-}
 
--- | Jeffrey Vitter's Algorithm R <http://www.cs.umd.edu/~samir/498/vitter.pdf>, suitable for unweighted sampling from a collections of unknown size.
+{-|
+ - Module      : System.Random.Reservoir
+ - Description : Reservoir Samplers
+ - Copyright   : (c) Mark Hay, 2018
+ - License     : BSD3
+ - Maintainer  : mah6@williams.edu
+ - Stability   : experimental
+ -
+ - Reservoir algorithms suitable for sampling from data streams of large or unknown size.
+ -}
 module System.Random.Reservoir 
     ( 
-      Res()
-    , reservoirSample
+    -- * Unweighted Sampling
+      Res(..)
     , getSample
-    , getSeed
-    , setSeed
     , emptyRes
     , emptyRes'
+    , reservoirSample
     ) where
 
 import Control.Monad.ST
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as M
+import Data.Word
 import System.Random
 import System.Random.Mersenne.Pure64
 
+-- | Wrapper for the state kept by algorithm R. 
+-- | Keeps the sample as an @IntMap@, the number of elements seen, and the random seed.
 data Res g a = Res
-    {   resSample :: !(IntMap a)
+    {  
+    -- | The current sample. Should usually not be modified by hand.
+        resSample :: !(IntMap a)
+    -- | The number of elements seen. Should almost never be modified by hand.
     ,   numSeen :: !Int
+    -- | The random seed for the sample
     ,   resSeed :: !g
     } deriving (Show)
 
 instance Functor (Res g) where
     fmap f r@(Res s _ _) = r{ resSample = fmap f s }
 
+-- | Extracts the sample as a list.
 getSample :: Res g a -> [a]
 getSample = fmap snd . M.toList . resSample
+{-# INLINE getSample #-}
 
-getSeed :: Res g a -> g
-getSeed = resSeed
-
-setSeed :: RandomGen g => g -> Res g a -> Res g a
-setSeed s res = res{resSeed = s}
-
+-- | Creates a @Res@ with nothing in the sample and with the counter at zero.
 emptyRes :: RandomGen g => g -> Res g a
 emptyRes = Res M.empty 0
+{-# INLINE emptyRes #-}
 
-emptyRes' :: Res PureMT a
-emptyRes' = emptyRes (pureMT 0)
+-- | A version of @emptyRes@ specialized to @System.Random.Mersenne.Pure64@.
+emptyRes' :: Word64 -> Res PureMT a
+emptyRes' n = emptyRes (pureMT n)
+{-# INLINE emptyRes' #-}
 
-reservoirSample :: (RandomGen g) => Int -> a -> Res g a -> Res g a
+-- | Jeffrey Vitter's "Algorithm R". Given the elements in the existing sample and a new element,
+-- | every element has a equal probability of being selected.
+-- | Intended to be partially applied on the sample size and used with @fold@-like functions to sample 
+-- | large streams of data.
+reservoirSample :: (RandomGen g) 
+    => Int -- ^ The maximum number of elements to be in the sample.
+    -> a  -- ^ The next element to be considered
+    -> Res g a -- ^ The current wrapped sample 
+    -> Res g a
 reservoirSample !maxSize !x (Res samp seen seed)
     | seen < maxSize = Res (M.insert seen x samp) (seen + 1) seed
     | otherwise = Res samp' (seen + 1) seed'
@@ -52,3 +75,4 @@ reservoirSample !maxSize !x (Res samp seen seed)
         samp' = if r' < maxSize
             then M.insert r' x samp
             else samp 
+{-# INLINEABLE reservoirSample #-}
